d2c530584a9848f5ab10f1494756215eb44a22f1
jerry <jerry871002@gmail.com>
2019-11-01 17:50:54 +0800

    add a lot of contents

--- analog.ino
+++ analog.ino
@@ -1,21 +1,20 @@
-// declare the pin we need
 int ledPin = 3;
 int potPin = A0;
 
 void setup() {
   // setup pin modes
-  pinMode(ledPin, OUTPUT);                  
+  pinMode(ledPin, OUTPUT);
   pinMode(potPin, INPUT);
 }
 
 void loop() {
   // read the value of the pot and store it as potValue
   int potValue = analogRead(potPin);
-  
+
   // turn led on and wait for the time equal to potValue
   digitalWrite(ledPin, HIGH);
   delay(potValue);
-  
+
   // re-read the value of the pot and store it as potValue
   potValue = analogRead(potPin);
 
